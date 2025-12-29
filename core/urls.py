423edb046a3863cd04a77d1b9dc7a69bc0a384e0
hari kumar <harikumar809816@gmail.com>
2025-12-29 12:38:28 +0530

    update in index.py

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('stations/<str:date>/', views.get_stations_by_date, name='get_stations_by_date'),
+    path('stations/<str:date>', get_stations_by_date),
 ]
--- geodetic/settings.py
+++ geodetic/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0z8iv+txm*#tu+i643tf4++fj$420&hj@a**hpd2bi!5@gudbc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
